2cc9e343bd333435af442e0fb2a4e46ddc61deb9
Birk <rgeebt01@yahoo.com>
2022-04-19 13:39:16 -0400

    changed output

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ A program that modifies the original "Hello World" to show some uses of Strings
 # Update1
 Added changes to what shows on the screen when the program is run.
 
+# Update2
+Changed "Hello World" to "C:\Windows\..."
--- src/main/java/Strings.java
+++ src/main/java/Strings.java
@@ -2,7 +2,7 @@ import java.util.Locale;
 
 public class Strings {
     public static void main(String[] args) {
-        String message = "  Hello \"World\"" + "!!";
+        String message = "  C:\\Windows\\...";
         System.out.println(message.replace("!", "*"));
         System.out.println(message.toLowerCase(Locale.ROOT));
         System.out.println(message.trim());
