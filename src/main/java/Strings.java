5a2bcb5b46bdbf207090ebb7f4bcf3c6d541f555
Birk <rgeebt01@yahoo.com>
2022-04-19 13:32:18 -0400

    updated hello world to discuss strings

---
+++ src/main/java/Strings.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+
+public class Strings {
+    public static void main(String[] args) {
+        String message = "  Hello World" + "!!";
+        System.out.println(message.replace("!", "*"));
+        System.out.println(message.toLowerCase(Locale.ROOT));
+        System.out.println(message.trim());
+        System.out.println(message);
+    }
+}
