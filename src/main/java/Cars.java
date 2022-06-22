dbb934af0d7f878491f5307789960f674e7d2936
Birk <rgeebt01@yahoo.com>
2022-06-22 12:49:24 -0400

    updated to include string methods and POJO class

---
+++ src/main/java/Cars.java
@@ -0,0 +1,78 @@
+public class Cars {
+    private String make, model, color;
+    private int year, numWheels;
+    private boolean hybrid, electric;
+    private double price, mileage;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getNumWheels() {
+        return numWheels;
+    }
+
+    public void setNumWheels(int numWheels) {
+        this.numWheels = numWheels;
+    }
+
+    public boolean isHybrid() {
+        return hybrid;
+    }
+
+    public void setHybrid(boolean hybrid) {
+        this.hybrid = hybrid;
+    }
+
+    public boolean isElectric() {
+        return electric;
+    }
+
+    public void setElectric(boolean electric) {
+        this.electric = electric;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
+}
--- src/main/java/Strings.java
+++ src/main/java/Strings.java
@@ -1,11 +1,95 @@
 import java.util.Locale;
 
 public class Strings {
+    static String str = "Learn string handling";
     public static void main(String[] args) {
         String message = "  C:\tWindows\\...";
         System.out.println(message.replace("!", "*"));
         System.out.println(message.toLowerCase(Locale.ROOT));
         System.out.println(message.trim());
         System.out.println(message);
+        var ans1 = ex1();
+        System.out.println(ans1);
+        var ans2=ex2();
+        System.out.println(ans2);
+        var ans3=ex3(10,15);
+        System.out.println(ans3);
+        var ans4=ex4(0,5);
+        System.out.println(ans4);
     }
+    // 1) Using the string "Learn string handling": what index is the last letter in the string?
+    private static int ex1() {
+
+        return str.length();
+    }
+    // 2) Using the same string as above, what character is located at the 10th index?
+    private static char ex2() {
+        return str.charAt(10);
+    }
+    // 3)Do a check to see if the character at index 10 and 15 the same?
+    private static boolean ex3(int x, int y){
+        String[] strArr=str.trim().split("");
+        if (strArr[x].equalsIgnoreCase(strArr[y])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // 4) Create a scenario that returns false if the character at the 0 index equals the 5 index
+    private static boolean ex4(int x, int y) {
+        return ex3(x,y);
+    }
+    //length()
+    //equals()
+    //equalsIgnoreCase()
+    //charAt()
+    //contains()
+    //substring()
+    //startsWith
+    //endsWith
+    //toLowerCase()
+    //toUpperCase()
+
+
+//            String str = "Learn string handling";
+//            String allUpper = "LEARN STRING HANDLING";
+//            String subStr = "";
+//
+//            boolean isEqualsTrue = false;
+//            boolean isEqualsIgnoreCaseTrue = false;
+//
+//            //This will return the first char of the string
+//            char ch1 = str.charAt(0);
+//            char ch2 = str.charAt(1);
+//            char ch3 = str.charAt(4);
+//
+//            if(str.equals(allUpper)) {
+//                isEqualsTrue = true;
+//            }else {
+//                isEqualsTrue = false;
+//            }
+//
+//            if(str.equalsIgnoreCase(allUpper)) {
+//                isEqualsIgnoreCaseTrue = true;
+//            }else {
+//                isEqualsIgnoreCaseTrue = false;
+//            }
+//
+//            subStr = str.substring(0, 6).trim();
+//
+//            System.out.println("Character at 0 index is: " + ch1);
+//            System.out.println("Character at 1 index is: " + ch2);
+//            System.out.println("Character at 4 index is: " + ch3);
+//
+//            System.out.println("Are str and allUpper equals- Ignore Case  : " + isEqualsIgnoreCaseTrue);
+//            System.out.println("Are str and allUpper equals  : " + isEqualsTrue);
+//
+//            System.out.println("Original string: " + str + " - Substring :" + subStr);
+//
+
+
+
+
+
+
 }
